2493f95b9898cd28b0cc5256fe94ff388652ef15
Camila_Sales <camilapsales27@gmail.com>
2018-05-14 23:36:21 -0300

    Update test changing encoding to utf-8

--- feature/xmits.py
+++ feature/xmits.py
@@ -1,5 +1,4 @@
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+# coding: utf-8
 
 import nltk
 
@@ -10,19 +9,23 @@ def extract_key_words(sentence, diagram_words):
 
     sentences = sent_tokenize.tokenize(sentence)
 
-    result = {'noun': {}, 'article': {}, 'verb': {}, 'adjective': {}}
+    result = {'noun': {}, 'article': {}, 'adverb':{}, 'verb': {}, 'adjective': {}}
+    result_intersection = {'noun': {}, 'article': {}, 'adverb':{}, 'verb': {}, 'adjective': {}}
 
-    with open('punctuation.txt', 'r') as f:
+    with open('punctuation.txt', 'r', encoding = 'utf8') as f:
         punctuation = f.read().split('\n')
 
     with open('article.txt', 'r') as f:
         article = f.read().split('\n')
 
-    with open('verb.txt', 'r') as f:
+    with open('verb.txt', 'r', encoding = 'utf8') as f:
         verb = f.read().split('\n')
 
     with open('adjective.txt', 'r') as f:
         adjective = f.read().split('\n')
+
+    with open('adverb.txt', 'r') as f:
+        adverb = f.read().split('\n')
 
     for sentence in sentences:
 
@@ -38,13 +41,16 @@ def extract_key_words(sentence, diagram_words):
             elif word in article:
                 tmp_class = 'article'
 
-            elif word in verb:
-                tmp_class = 'verb'
-
             elif word in adjective:
                 tmp_class = 'adjective'
 
+            elif word in adverb:
+                tmp_class = 'adverb'
+
+            elif word in verb:
+                tmp_class = 'verb'
             else:
+                print(verb)
                 tmp_class = 'noun'
 
             if tmp_class is not None:
@@ -62,3 +68,5 @@ def update_dict(dictionary, key, word):
     dictionary[key][word] = 1 if word not in dictionary[key] else dictionary[key][word] + 1
 
 
+def update_dict_intersect(dictionary, key, word):
+    dictionary[key][word] = 1 if word not in dictionary[key] else dictionary[key][word] + 1
--- test/test_feature_extraction.py
+++ test/test_feature_extraction.py
@@ -1,22 +1,19 @@
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
-
+# coding: utf-8
 import unittest
 import feature
-
 
 class TestFeatureExtractor(unittest.TestCase):
 
     def test_feature_extractor(self):
 
-        result = feature.extract_key_words('O Lucas é legal.', diagram_words)
+        result = feature.extract_key_words('O cesto contém legal.',"O deste deve era conter.")
 
         print(result)
 
-        self.assertEquals(result['noun']['lucas'], 1)
+        self.assertEquals(result['noun']['cesto'], 1)
 
         self.assertEquals(result['article']['o'],  1)
 
-        self.assertEquals(result['verb']['é'], 1)
+        self.assertEquals(result['verb']['contém'], 1)
 
         self.assertEqual(result['adjective']['legal'], 1)
